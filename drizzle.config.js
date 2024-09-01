ae2ea03239ced6764878978265bedf67b6baa6ad
stanleman <stanlord2009@gmail.com>
2024-09-01 18:26:02 +0800

    drizzle config url

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default {
   schema: "./utils/schema.jsx",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://nwealthdb_owner:WqbK67ERasQn@ep-round-morning-a1rwr4b3.ap-southeast-1.aws.neon.tech/nwealthdb?sslmode=require",
+    url: "replace with your database url",
   },
 };
